e5dbfb3ec3a034054f713ddd16488f7999847644
Davi Carvalho <davicfg@gmail.com>
2020-06-08 22:27:14 -0300

    Frist commit

---
+++ src/main/java/br/com/cdc/apicdc/ApiDaCasaDoCódigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cdc.apicdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDaCasaDoCódigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDaCasaDoCódigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cdc/apicdc/ApiDaCasaDoCódigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cdc.apicdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDaCasaDoCódigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
