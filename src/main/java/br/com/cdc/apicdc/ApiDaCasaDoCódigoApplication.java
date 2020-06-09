4e87fbe166f6e4f93dea1b0fdf0519db03ee2c6e
davicfg <davicfg@hotmail.com>
2020-06-09 00:01:12 -0300

    Configurando o swagger no projeto

--- src/main/java/br/com/cdc/apicdc/ApiDaCasaDoCódigoApplication.java
+++ src/main/java/br/com/cdc/apicdc/ApiDaCasaDoCódigoApplication.java
@@ -2,8 +2,13 @@ package br.com.cdc.apicdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @SpringBootApplication
+@ComponentScan("br.com.cdc.apicdc")
 public class ApiDaCasaDoCódigoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/cdc/shared/configuration/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.cdc.shared.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfiguration {
+
+	@Bean
+	public Docket api() {
+		Predicate<RequestHandler> basePackage = RequestHandlerSelectors.basePackage("br.com.cdc.apicdc");
+		Predicate<String> apiUrls = PathSelectors.ant("/api/**");
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(basePackage).paths(apiUrls).paths(Predicates.not(PathSelectors.regex("/error.*"))).build();
+
+	}
+}
